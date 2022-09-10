2cbb62829fa42566554769784a9a1eaf3e8e8abd
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-11 01:32:32 +0300

    Fetching and Displaying image on page success

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 document.getElementsByClassName("dogo-img");
 generate_query=document.querySelector(".generate_btn");
+getdogimage_query=document.querySelector(".dogo-img");
 generate_query.addEventListener("click",generating_prediction);
 
 
@@ -7,8 +8,14 @@ generate_query.addEventListener("click",generating_prediction);
     fetch("https://dog.ceo/api/breeds/image/random")
     .then(response=> response.json())
     .then((data) => {
-      console.log(data)
+
+      var dogimage=data.message;
+      console.log(dogimage)
+      getdogimage_query.src=dogimage
+
     })
+
+
 
 function generating_prediction()
 {
