23f48ba7a6ec683c00507577d41817966f0c9c71
Mazennaja123 <63180234+Mazennaja123@users.noreply.github.com>
2022-09-11 01:18:10 +0300

    testing image API and checking data received

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
-document.getElementsByClassName("dogo-img")
+document.getElementsByClassName("dogo-img");
+generate_query=document.querySelector(".generate_btn");
+generate_query.addEventListener("click",generating_prediction);
 
 
 
     fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response =>response.json())
-    .then(JSON=> dogo-img.src=JSON.message())
+    .then(response=> response.json())
+    .then((data) => {
+      console.log(data)
+    })
+
+function generating_prediction()
+{
+console.log("test")
+
+}
